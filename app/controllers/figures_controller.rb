6d60e24ba2e7b8019d6e94e8de5c4bc5ffb08b7b
Zac Willmington <zac.willmington@gmail.com>
2018-06-15 18:44:19 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,11 +1,18 @@
 class FiguresController < ApplicationController
-    
+
     get '/figures' do
+        binding.pry
+        @figures = Figure.all
         erb :'figures/index'
     end
 
     get '/figures/new' do
         erb :'figures/new'
+    end
+
+    post '/figures/new' do
+        binding.pry
+        erb :'figures/show'
     end
 
 end
