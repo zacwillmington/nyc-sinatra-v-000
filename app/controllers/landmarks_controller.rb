6aedaae676f96ecaf8aefca42f490aff7cf40527
Zac Willmington <zac.willmington@gmail.com>
2018-06-16 19:58:14 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,10 @@
 class LandmarksController < ApplicationController
-    
+
+    get '/landmarks' do
+        erb :'/landmarks/index'
+    end
+
+    get '/landmarks/new' do
+        erb '/landmarks/new'
+    end
 end
