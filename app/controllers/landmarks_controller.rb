656c957bb9b8725845693ed9f8ddc3f361874daf
Zac Willmington <zac.willmington@gmail.com>
2018-06-16 20:00:06 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,11 +5,15 @@ class LandmarksController < ApplicationController
     end
 
     get '/landmarks/new' do
-        
+
         erb :'/landmarks/new'
     end
 
     post '/landmarks' do
         redirect to :'/landmarks/#{@landmark.id}'
+    end
+
+    get '/landmarks/:id' do
+        erb :'landmarks/show'
     end
 end
