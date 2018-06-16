3fb175e00d41543c2763a18757704d6a594bc9ac
Zac Willmington <zac.willmington@gmail.com>
2018-06-16 20:15:23 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -17,7 +17,9 @@ class LandmarksController < ApplicationController
     end
 
     get '/landmarks/:id/edit' do
-        
+        @landmark = Landmark.find(params[:id])
+
+        erb :'/landmarks/edit'
     end
 
     get '/landmarks/:id' do
